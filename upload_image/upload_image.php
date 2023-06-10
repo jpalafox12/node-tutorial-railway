3b48be3f7ca7ddb6f4fec7c999a4cdb42d687aff
jpalafox12 <53842278+jpalafox12@users.noreply.github.com>
2023-06-10 17:52:02 -0600

    Add files via upload

---
+++ upload_image/upload_image.php
@@ -0,0 +1,32 @@
+<?php
+//include './conexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id_image = $_POST['idImage'];
+    $nom_image = $_POST['nomImage'];
+    $image = $_POST['image'];
+
+    $path = "img/$nom_image.png";
+    $actual_path = "http://localhost/upload_image/$path";
+
+    // Verificar si el archivo existe y eliminarlo si es necesario
+    if (file_exists($path)) {
+        unlink($path);
+    }
+
+    // Guardar la nueva imagen
+    file_put_contents($path, base64_decode($image));
+
+    $message = 'Se agregó correctamente la imagen';
+
+    $response = array(
+        "message" => $message
+    );
+
+    $json_response = json_encode($response);
+
+    header('Content-Type: application/json');
+
+    echo $json_response;
+}
+?>
